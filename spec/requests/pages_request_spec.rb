967842e6927d5fda09f152e4c0845b8ab88e1edc
irah rosete <irah.rosete@outlook.com>
2021-03-07 01:53:53 +1100

    add homepage response tests, and fix navbar links

--- spec/requests/pages_request_spec.rb
+++ spec/requests/pages_request_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-
+  describe "HOMEPAGE" do
+    before(:each) do
+      get "/"
+    end
+    it "should respond with a status of 200" do
+      expect(response).to have_http_status(:success)
+    end
+    it "should render pages/index page" do
+      expect(response).to render_template("index")
+    end
+  end
 end
