fb5c999e1db2df65a8f8ecfafe4f3df003f89a7e
irah rosete <irah.rosete@outlook.com>
2021-03-14 20:10:36 +1100

    implement admin function to manage users and add link to dashboard from the navbar

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'simple_form'
 gem 'devise'
 gem 'font-awesome-rails'
 gem 'cancancan'
+gem "administrate"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      # resources :cideries
+      # resources :drinks
+
+      root to: "users#index"
+    end
   devise_for :users
   root to: "pages#index"
   resources :drinks
